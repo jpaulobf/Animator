3aaf7090e6ed259322c64a38ff685bd7ddf11505
jpaulobf <jpaulobf@gmail.com>
2026-03-30 22:01:45 +0100

    Refactory

---
+++ src/br/com/game/animator/game/gameUI/advertise/DeveloperAdvertise.java
@@ -0,0 +1,9 @@
+package br.com.game.animator.game.gameUI.advertise;
+
+import br.com.game.animator.game.gameUI.CoreGameLogic;
+
+public interface DeveloperAdvertise extends CoreGameLogic {
+
+	public final static Float MAX_COUNTER_VALUE = 300f;
+	
+}
