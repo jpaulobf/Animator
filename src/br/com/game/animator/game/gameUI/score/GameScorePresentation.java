84c73c072313dc4bc9fac0ede29cf4b458069393
jpaulobf <jpaulobf@gmail.com>
2026-03-30 22:02:29 +0100

    Refactory

---
+++ src/br/com/game/animator/game/gameUI/score/GameScorePresentation.java
@@ -0,0 +1,12 @@
+package br.com.game.animator.game.gameUI.score;
+
+import br.com.game.animator.game.gameUI.CoreGameLogic;
+
+/**
+ * @author João Paulo Faria
+ */
+public interface GameScorePresentation extends CoreGameLogic {
+
+	public final static Float MAX_HI_SCORE_COUNTER_VALUE = 1680f;
+
+}
