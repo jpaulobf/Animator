123d8e655a081f19a10fe81f04b006fb045f4a17
jpaulobf <jpaulobf@gmail.com>
2026-03-30 22:01:57 +0100

    Refactory

---
+++ src/br/com/game/animator/game/gameUI/loading/Loading.java
@@ -0,0 +1,12 @@
+package br.com.game.animator.game.gameUI.loading;
+
+import br.com.game.animator.game.gameUI.CoreGameLogic;
+
+/**
+ * @author João Paulo Faria
+ *
+ */
+public interface Loading extends CoreGameLogic {
+
+	
+}
