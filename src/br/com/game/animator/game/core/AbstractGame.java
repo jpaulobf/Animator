9ea34bc6851792a296ddb68d1896f7c30ac7438c
jpaulobf <jpaulobf@gmail.com>
2026-04-04 21:34:04 +0100

    StateMachine for the Game (WIP)

--- src/br/com/game/animator/game/core/AbstractGame.java
+++ src/br/com/game/animator/game/core/AbstractGame.java
@@ -7,29 +7,32 @@ import br.com.game.animator.engine.GameEngine;
 import br.com.game.animator.window.Window;
 
 /**
- * AbstractGame - Abstract class that implements the IGame interface and provides common functionality for all games.
- * This class is responsible for managing the game loop, handling the game window, and providing methods for pausing, resuming, and stopping the game.
- * It also defines abstract methods for updating the game state, rendering the game, and handling key presses, which must be implemented by any 
+ * AbstractGame - Abstract class that implements the IGame interface and
+ * provides common functionality for all games.
+ * This class is responsible for managing the game loop, handling the game
+ * window, and providing methods for pausing, resuming, and stopping the game.
+ * It also defines abstract methods for updating the game state, rendering the
+ * game, and handling key presses, which must be implemented by any
  * concrete game class that extends this abstract class.
  */
 public abstract class AbstractGame implements IGame {
 
-    //--- Properties ---//
-    protected Graphics2D graphics2D = null;
-    protected Window gameWindow = null;
-    protected GameEngine gameEngine = null;
-    protected volatile boolean running = false;
+	// --- Properties ---//
+	protected Graphics2D graphics2D = null;
+	protected Window gameWindow = null;
+	protected GameEngine gameEngine = null;
+	protected volatile boolean running = false;
 	protected volatile boolean gameOver = false;
 	protected volatile boolean isPaused = false;
 	protected volatile boolean loading = false;
-    protected volatile boolean isToShowFPS = true;
+	protected volatile boolean isToShowFPS = true;
 
-    /**
-     * Constructor
-     */
-    public AbstractGame() {
-		//do nothing
-    }
+	/**
+	 * Constructor
+	 */
+	public AbstractGame() {
+		// do nothing
+	}
 
 	public void startGame() {
 		this.startGame(60);
@@ -45,7 +48,7 @@ public abstract class AbstractGame implements IGame {
 
 	public abstract void init();
 
-    /**
+	/**
 	 * paintScreen - Paint the buffer to the screen.
 	 */
 	public void paintScreen() {
@@ -64,43 +67,51 @@ public abstract class AbstractGame implements IGame {
 		}
 	}
 
-    /**
-     * Pause the game.
-     */
+	/**
+	 * Pause the game.
+	 */
 	public void pauseGame() {
 		this.isPaused = true;
 	}
 
-    /**
-     * loading - Set the loading flag to true, indicating that the game is currently loading resources or performing some initialization tasks.
-     */
+	/**
+	 * loading - Set the loading flag to true, indicating that the game is currently
+	 * loading resources or performing some initialization tasks.
+	 */
 	public void loading() {
 		this.loading = true;
 	}
 
-    /**
-     * loadingDone - Set the loading flag to false, indicating that the game has finished loading resources or initialization tasks.
-     */
+	/**
+	 * loadingDone - Set the loading flag to false, indicating that the game has
+	 * finished loading resources or initialization tasks.
+	 */
 	public void loadingDone() {
 		this.loading = false;
 	}
 
-    /**
-     * Resume the game.
-     */
+	/**
+	 * Resume the game.
+	 */
 	public void resumeGame() {
 		this.isPaused = false;
 	}
 
-    /**
-     * Stop the game.
-     */
+	/**
+	 * Stop the game.
+	 */
 	public void stopGame() {
 		gameEngine.stop();
 	}
 
-    //--- Abstract Methods ---//
-    public abstract void update(long frametime);
-    public abstract void render(long delta);
-    public abstract void keyPressed(int keyCode, boolean isAltDown);
+	public Window getGameWindow() {
+		return gameWindow;
+	}
+
+	// --- Abstract Methods ---//
+	public abstract void update(long frametime);
+
+	public abstract void render(long delta);
+
+	public abstract void keyPressed(int keyCode, boolean isAltDown);
 }
