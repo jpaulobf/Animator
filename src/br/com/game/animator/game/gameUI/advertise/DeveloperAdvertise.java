134832251ad4ef635a7a4179dbe858741bd6e9b2
jpaulobf <jpaulobf@gmail.com>
2026-04-04 21:34:15 +0100

    StateMachine for the Game (WIP)

--- src/br/com/game/animator/game/gameUI/advertise/DeveloperAdvertise.java
+++ src/br/com/game/animator/game/gameUI/advertise/DeveloperAdvertise.java
@@ -2,8 +2,15 @@ package br.com.game.animator.game.gameUI.advertise;
 
 import br.com.game.animator.game.gameUI.CoreGameLogic;
 
+/**
+ * Interface that represents the logic of the developer advertisement in the game.
+ * It extends the CoreGameLogic interface, which means it must implement the methods defined in that interface.
+ * 
+ * @author João Paulo Faria
+ */
 public interface DeveloperAdvertise extends CoreGameLogic {
 
+	// --- Constants
 	public final static Float MAX_COUNTER_VALUE = 300f;
 	
 }
