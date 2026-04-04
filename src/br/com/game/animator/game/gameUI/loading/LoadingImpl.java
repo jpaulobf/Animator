c471653ed29113230897e51f332a4d873160cda7
jpaulobf <jpaulobf@gmail.com>
2026-04-04 21:34:40 +0100

    StateMachine for the Game (WIP)

--- src/br/com/game/animator/game/gameUI/loading/LoadingImpl.java
+++ src/br/com/game/animator/game/gameUI/loading/LoadingImpl.java
@@ -9,79 +9,80 @@ import br.com.game.animator.util.ImageUtil;
  * @author João Paulo Faria
  */
 public class LoadingImpl implements Loading {
-	
-	//--- Properties
-	private Integer loadingCounter	= 0;
-	private BufferedImage loading1	= null;
-	private BufferedImage loading2	= null;
-	private BufferedImage loading3	= null;
-	private BufferedImage loading4	= null;
-	private Integer PWIDTH			= null;
-	private Integer PHEIGHT			= null;
-	private ImageUtil imageUtil		= null;
-	private Integer positionX		= 0;
-	private Integer positionY		= 0;
-	private Integer imageWidth		= 0;
-	private Integer imageHeight		= 0;
-	private Integer currentStatus	= 0;
-	
+
+	// --- Properties
+	private Integer loadingCounter = 0;
+	private BufferedImage loading1 = null;
+	private BufferedImage loading2 = null;
+	private BufferedImage loading3 = null;
+	private BufferedImage loading4 = null;
+	private Integer PWIDTH = null;
+	private Integer PHEIGHT = null;
+	private ImageUtil imageUtil = null;
+	private Integer positionX = 0;
+	private Integer positionY = 0;
+	private Integer imageWidth = 0;
+	private Integer imageHeight = 0;
+	private Integer currentStatus = 0;
+
 	/**
 	 * Constructor
+	 * 
 	 * @param pwidth
 	 * @param pheight
 	 * @param currentAspectRatio
 	 */
 	public LoadingImpl(Integer pwidth, Integer pheight, Integer currentAspectRatio) {
-		this.PWIDTH 		= pwidth;
-		this.PHEIGHT 		= pheight;
-		this.imageUtil 		= new ImageUtil(pwidth, pheight, currentAspectRatio);
-		this.loading1 		= this.imageUtil.loadScaledImage("/res/images/loading_1a.png");
-		this.loading2 		= this.imageUtil.loadScaledImage("/res/images/loading_2a.png");
-		this.loading3 		= this.imageUtil.loadScaledImage("/res/images/loading_3a.png");
-		this.loading4 		= this.imageUtil.loadScaledImage("/res/images/loading_4a.png");
-		this.imageWidth		= this.loading1.getWidth();
-		this.imageHeight	= this.loading1.getHeight();
-		this.positionX		= (this.PWIDTH / 2) - (this.imageWidth / 2);
-		this.positionY		= (this.PHEIGHT / 2) - (this.imageHeight / 2);
+		this.PWIDTH = pwidth;
+		this.PHEIGHT = pheight;
+		this.imageUtil = new ImageUtil(pwidth, pheight, currentAspectRatio);
+		this.loading1 = this.imageUtil.loadScaledImage("/res/images/loading_1a.png");
+		this.loading2 = this.imageUtil.loadScaledImage("/res/images/loading_2a.png");
+		this.loading3 = this.imageUtil.loadScaledImage("/res/images/loading_3a.png");
+		this.loading4 = this.imageUtil.loadScaledImage("/res/images/loading_4a.png");
+		this.imageWidth = this.loading1.getWidth();
+		this.imageHeight = this.loading1.getHeight();
+		this.positionX = (this.PWIDTH / 2) - (this.imageWidth / 2);
+		this.positionY = (this.PHEIGHT / 2) - (this.imageHeight / 2);
 	}
-	
+
 	public void draw(Graphics2D g2d) {
 		g2d.setColor(Color.LIGHT_GRAY);
 		g2d.fillRect(0, 0, this.PWIDTH, this.PHEIGHT);
-		
+
 		if (this.currentStatus == 0) {
-			g2d.drawImage(this.loading1, 
-						  this.positionX,
-						  this.positionY,
-						  this.imageWidth, 
-						  this.imageHeight, 
-						  null);
+			g2d.drawImage(this.loading1,
+					this.positionX,
+					this.positionY,
+					this.imageWidth,
+					this.imageHeight,
+					null);
 		} else if (this.currentStatus == 1) {
-			g2d.drawImage(this.loading2, 
-					  	  this.positionX,
-					  	  this.positionY,
-					  	  this.imageWidth, 
-					  	  this.imageHeight, 
-					  	  null);
+			g2d.drawImage(this.loading2,
+					this.positionX,
+					this.positionY,
+					this.imageWidth,
+					this.imageHeight,
+					null);
 		} else if (this.currentStatus == 2) {
-			g2d.drawImage(this.loading3, 
-					  	  this.positionX,
-					  	  this.positionY,
-					  	  this.imageWidth, 
-					  	  this.imageHeight, 
-					  	  null);
+			g2d.drawImage(this.loading3,
+					this.positionX,
+					this.positionY,
+					this.imageWidth,
+					this.imageHeight,
+					null);
 		} else {
-			g2d.drawImage(this.loading4, 
-					  	  this.positionX,
-					  	  this.positionY,
-					  	  this.imageWidth, 
-					  	  this.imageHeight, 
-					  	  null);
+			g2d.drawImage(this.loading4,
+					this.positionX,
+					this.positionY,
+					this.imageWidth,
+					this.imageHeight,
+					null);
 		}
 	}
 
 	public void update(long frametime) {
-		this.loadingCounter = (this.loadingCounter + 2) % 400;  
+		this.loadingCounter = (this.loadingCounter + 2) % 400;
 		if (this.loadingCounter >= 0 && this.loadingCounter < 100) {
 			this.currentStatus = 0;
 		} else if (this.loadingCounter >= 100 && this.loadingCounter < 200) {
@@ -94,11 +95,11 @@ public class LoadingImpl implements Loading {
 	}
 
 	public void updateGraphics(boolean fullScreen, Integer pwidth, Integer pheight, Integer currentAspectRatio) {
-		this.PWIDTH 		= pwidth;
-		this.PHEIGHT 		= pheight;
-		this.imageUtil 		= new ImageUtil(pwidth, pheight, currentAspectRatio);
-		this.positionX		= (this.PWIDTH / 2) - (this.imageWidth / 2);
-		this.positionY		= (this.PHEIGHT / 2) - (this.imageHeight / 2);
+		this.PWIDTH = pwidth;
+		this.PHEIGHT = pheight;
+		this.imageUtil = new ImageUtil(pwidth, pheight, currentAspectRatio);
+		this.positionX = (this.PWIDTH / 2) - (this.imageWidth / 2);
+		this.positionY = (this.PHEIGHT / 2) - (this.imageHeight / 2);
 	}
 
 	public void resetCounters() {
