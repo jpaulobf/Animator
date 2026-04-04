575914127755c4748fa56589d1e0fc19cad3fecc
jpaulobf <jpaulobf@gmail.com>
2026-04-04 21:34:27 +0100

    StateMachine for the Game (WIP)

--- src/br/com/game/animator/game/gameUI/intro/GameIntro.java
+++ src/br/com/game/animator/game/gameUI/intro/GameIntro.java
@@ -1,21 +1,10 @@
 package br.com.game.animator.game.gameUI.intro;
 
-import java.awt.Graphics2D;
-
 import br.com.game.animator.game.gameUI.CoreGameLogic;
 
 /**
  * @author João Paulo Faria
  */
 public interface GameIntro extends CoreGameLogic {
-	
-	public final static Float MAX_COUNTER_SUBINTRO_VALUE = 900f;
 
-	public boolean subIntroFinished();
-	
-	public void updateSubIntro();
-	
-	public void drawSubIntro(Graphics2D g2d);
-
-	public void updateCanvasPropertiesSubIntro(boolean fullScreen, Integer pwidth, Integer pheight, Integer currentAspectRatio);
 }
