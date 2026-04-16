9757067547ab06dd39439033714301e9fe441b64
jpaulobf <jpaulobf@gmail.com>
2026-04-16 19:39:50 +0100

    Avoid SO scaling

--- src/main/java/br/com/animator/Launcher.java
+++ src/main/java/br/com/animator/Launcher.java
@@ -8,11 +8,10 @@ import br.com.animator.game.Game;
  */
 public class Launcher {
     	public static void main(String args[]) {
-		SwingUtilities.invokeLater(new Runnable() {
-
-			public void run() {
-				new Game().startGame(60);
-			}
-		});
+			SwingUtilities.invokeLater(new Runnable() {		
+				public void run() {
+					new Game().startGame(0);
+				}
+			});
 	}
 }
