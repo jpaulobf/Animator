b085f6d2c752d76e2000749670b146c4e368bf83
jpaulobf <jpaulobf@gmail.com>
2026-04-17 19:42:34 +0100

    Launcher

--- src/main/java/br/com/animator/Launcher.java
+++ src/main/java/br/com/animator/Launcher.java
@@ -7,11 +7,11 @@ import br.com.animator.game.Game;
  * Launcher class to start the game engine.
  */
 public class Launcher {
-    	public static void main(String args[]) {
-			SwingUtilities.invokeLater(new Runnable() {		
-				public void run() {
-					new Game().startGame(0);
-				}
-			});
+	public static void main(String args[]) {
+		SwingUtilities.invokeLater(new Runnable() {		
+			public void run() {
+				new Game().startGame(0);
+			}
+		});
 	}
 }
