0b39a7fddfda47969b3c4b0ea4bd9e831570876b
jpaulobf <jpaulobf@gmail.com>
2026-04-19 20:54:06 +0100

    Implemented high accuracy fps update

---
+++ src/main/java/br/com/animator/core/game/GameGlobals.java
@@ -0,0 +1,8 @@
+package br.com.animator.core.game;
+
+public class GameGlobals {
+
+    public static final double GAME_TARGET_VELOCITY = (double)((double)1D / (1_000_000_000L / 60L));
+    public static double GAME_VELOCITY = 0;
+
+}
